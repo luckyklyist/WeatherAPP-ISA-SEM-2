94884f024f2c6c34c9a78926644f59dbb103891a
luckyklyist <puspaanupam@gmail.com>
2023-08-11 19:41:56 +0545

    search added

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,17 @@
 <div class="flex flex-col items-center justify-center text-4xl font-bold text-gray-700 mt-8 mb-16">
-              <div class="mb-4">
-                  Weather App
-              </div>
-              <img src="https://uxwing.com/wp-content/themes/uxwing/download/weather/weather-icon.png" alt=""
-                  class="h-16">
-          </div>
+    <div class="mb-4">
+        Weather App
+    </div>
+    <img src="https://uxwing.com/wp-content/themes/uxwing/download/weather/weather-icon.png" alt="" class="h-16">
+</div>
 
-          <div class="my-8 flex items-center justify-center ">
-              <input type="text" placeholder="Enter city name" id="city-input"
-                  class="p-3 w-64 border border-gray-300 rounded-l-lg focus:outline-none focus:border-blue-500 placeholder-gray-400">
-              <button onclick="getWeather()"
-                  class="px-6 py-3 bg-green-500 text-white font-bold rounded-lg ml-2 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 shadow-md">
-                  Search
-              </button>
-          </div>
+<div class="my-8 flex items-center justify-center ">
+    <form method="post">
+        <input type="text" placeholder="Enter city name" id="city-input" name="city"
+            class="p-3 w-64 border border-gray-300 rounded-l-lg focus:outline-none focus:border-blue-500 placeholder-gray-400">
+        <button type="submit"
+            class="px-6 py-3 bg-green-500 text-white font-bold rounded-lg ml-2 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 shadow-md">
+            Search
+        </button>
+    </form>
+</div>
