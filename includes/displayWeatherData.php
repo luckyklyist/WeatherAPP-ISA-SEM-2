18f2269ad326f09edaaa710d266c1ab8184d7ed0
luckyklyist <puspaanupam@gmail.com>
2023-08-11 18:21:50 +0545

    feathweather data seprated

--- includes/displayWeatherData.php
+++ includes/displayWeatherData.php
@@ -1,22 +1,5 @@
 <?php
-                function fetchWeatherData($apiKey, $city)
-                {
-                    $apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}";
-
-                    $curl = curl_init($apiUrl);
-                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                    $apiResponse = curl_exec($curl);
-
-                    if ($apiResponse === false) {
-                        echo "Error fetching weather data from the API: " . curl_error($curl);
-                        curl_close($curl);
-                        exit();
-                    }
-
-                    curl_close($curl);
-
-                    return json_decode($apiResponse, true);
-                }
+               include 'fetchWeatherData.php';
 
                 function saveWeatherData($conn, $city, $weatherData)
                 {
